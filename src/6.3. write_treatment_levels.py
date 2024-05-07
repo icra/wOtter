171a7eaea6d76602b2d1c264d362895bbd83f551
Lluís Bosch <holalluis@gmail.com>
2024-05-07 16:46:00 +0200

    Update 6.3. write_treatment_levels.py

--- src/6.3. write_treatment_levels.py
+++ src/6.3. write_treatment_levels.py
@@ -16,9 +16,7 @@ AGG_WWTP_df['Treatment_level'][AGG_WWTP_df['Secondary'] == 1] = 2  # 2 secondary
 # define secondary + and secondary ++
 AGG_WWTP_df['Biological treatment'] = AGG_WWTP_df['NRemoval'] + AGG_WWTP_df['PRemoval']
 
-#AGG_WWTP_df['Treatment_level'][AGG_WWTP_df['Biological treatment'] == 1] = 3  # 3 is secondary plus
-#AGG_WWTP_df['Treatment_level'][AGG_WWTP_df['Biological treatment'] == 2] = 4  # 4 is secondary plus plus
-AGG_WWTP_df['Treatment_level'][AGG_WWTP_df['Ozonation'] == 1] = 3  # 5 is tertiary
+AGG_WWTP_df['Treatment_level'][AGG_WWTP_df['Ozonation'] == 1] = 3  # 3 is tertiary
 
 fields = ['pixel_number', 'Treatment_level', 'Treat_a', 'Filt_a', 'Unfilt_a', 'pollution', 'dcpLatitud', 'dcpLongitu',
           'countryID']
